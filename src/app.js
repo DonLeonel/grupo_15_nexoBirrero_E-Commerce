ac9deb368cbd11492584ff1dbd84a664e60f9cdd
maurichavarri <mauriciochavarri@gmail.com>
2023-11-14 19:18:58 -0300

    agregando archivos

--- src/app.js
+++ src/app.js
@@ -4,10 +4,36 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
-app.listen(3000, ()=> {
-    console.log("Servidor corriendo.");
+app.listen(3000, () => console.log('Servidor corriendo. http://localhost:3000/'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
 
-app.get('/', (req,res) => {
-    res.sendFile(path.resolve(__dirname, './views/index.html'));
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/register.html'));
+});
+
+app.get('/productos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productos.html'));
+});
+
+app.get('/detalle-producto', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/detalle-producto.html'));
+});
+
+app.get('/carrito', (req,res) => {
+    res.sendFile(path.join(__dirname, 'views/carrito.html'));
+});
+
+app.get('/recuperar-password', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/recuperar-password.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/not-found.html'));
 });
