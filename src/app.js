16f06a8930a865dd48fc7f12a3b434bf687dc54c
maurichavarri <mauriciochavarri@gmail.com>
2023-11-24 12:14:56 -0300

    implementando mvc

--- src/app.js
+++ src/app.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const homeRoute = require('./routes/homeRoute.js');
+const loginRoute = require('./routes/loginRoute.js');
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
 app.listen(3000, () => console.log('Servidor corriendo. http://localhost:3000/'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
-});
-
-app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/login.html'));
-});
+app.use('/', homeRoute);
+app.use('/login', loginRoute);
 
 app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/register.html'));
