de19085675766a16f232271c69b595967d30e6c4
maurichavarri <mauriciochavarri@gmail.com>
2023-12-08 12:40:57 -0300

    detallando

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -3,11 +3,13 @@ const fs = require('fs');
 
 module.exports = {
     index: (req, res) => {        
-        const productos = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/productos.json'), 'utf-8'));                
-         
-        res.render('productos', { afrutados, trigo , ale });
+        //const productos = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/productos.json'), 'utf-8'));                
+        
+        //res.render('productos', { afrutados, trigo , ale });
     },
-    detalle : (req, res) => {                        
-        res.render('detalle-producto');
+    detalle : (req, res) => { 
+
+        const productos = JSON.parse(fs.readFileSync(path.join(__dirname, '../data/productos.json'), 'utf-8'));
+        res.render('detalle-producto', { productos });
     }
 }
--- src/routes/productosRouter.js
+++ src/routes/productosRouter.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const productosController = require('../controllers/productosController');
 
 router.get('/producto/:categoria', productosController.index);
-router.get('/producto/:id', productosController.detalle )
+router.get('/producto/detalle-producto/:id', productosController.detalle);
 
 module.exports = router;
