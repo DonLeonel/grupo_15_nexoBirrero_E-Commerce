21f6d3c8729f772de5447643c23944a7c9c9c799
DonLeonel <leonel.don@hotmail.es>
2024-03-19 12:21:11 -0300

    correciones

--- src/controllers/api/usuariosAPIController.js
+++ src/controllers/api/usuariosAPIController.js
@@ -4,43 +4,67 @@ module.exports = {
     listado: async (req, res) => {
         try {
             const usuarios = await Usuario.findAll({
+                attributes: {
+                    exclude: ['contrasenia']
+                },
                 where: {
                     activo: 1
                 }
             });
 
             let respuesta = {
-                length: usuarios.length,
-                statusCode: 200,
+                meta: {
+                    length: usuarios.length,
+                    url: '/api/usuarios',
+                    statusCode: 200
+                },              
                 data: usuarios
             };
 
-            res.json(respuesta);
+            res.status(200).json(respuesta);
         } catch (error) {
-            throw new Error(error);
+            console.error('Error: ' ,error);
+            res.status(500).json({
+                error: error.message,
+                statusCode: 500
+            });
         }
     },
     detalle: async (req, res) => {
         try {
             const usuario = await Usuario.findByPk(req.params.id, {
+                attributes: {
+                    exclude: ['contrasenia']
+                },
                 include: ['rol', 'ciudad']
             });
 
-            if (usuario) {
-                delete usuario.contrasenia;
-                let respuesta = {
-                    statusCode: 200,
+            let respuesta;
+            if (usuario) {                
+                respuesta = {
+                    meta: {
+                        url: '/api/usuario/:id',
+                        statusCode: 200
+                    },
                     data: usuario
                 };
-                res.json(respuesta);
+                res.status(200).json(respuesta);
             } else {
-                let respuesta = {
-                    statusCode: 400,
+                respuesta = {
+                    meta: {
+                        url: '/api/usuario/:id',
+                        statusCode: 400                      
+                    },
                     data: null
                 };
+                res.status(400).json(respuesta);
             };
         } catch (error) {
-            throw new Error(error);
+            console.error('Error: ' ,error);
+            res.status(500).json({
+                error: error.message,
+                statusCode: 500
+            });
         }
     }
 }
