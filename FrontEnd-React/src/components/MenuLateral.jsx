c4f7a86c326ae3f4b215f989465d559697cdbda4
maurichavarri <mauriciochavarri@gmail.com>
2024-03-26 02:17:03 -0300

    CSS del Dashboard 2

--- FrontEnd-React/src/components/MenuLateral.jsx
+++ FrontEnd-React/src/components/MenuLateral.jsx
@@ -4,10 +4,14 @@ import { Link } from 'react-router-dom'
 export const MenuLateral = ({ to1, to2, nombre1, nombre2 }) => {
   return (
     <nav className='menuLateral'>
-      <ul>
-        <li><Link to={to1}>{nombre1}</Link></li>
-        <li><Link to={to2}>{nombre2}</Link></li>
-      </ul>
+      <section>
+        <div>
+          <Link to={to1} className='react-link'>{nombre1}</Link>
+        </div>
+        <div>
+          <Link to={to2} className='react-link'>{nombre2}</Link>
+        </div>
+      </section>
     </nav>
   )
 }
